6a2e085039b4610ad72b300880d4e41d4486d093
specialprojectaalto5g <specialprojectaalto5g@gmail.com>
2024-05-20 15:16:27 +0300

    server.sh

--- server.sh
+++ server.sh
@@ -8,3 +8,6 @@ iperf3 -s -p 5202 -B 0.0.0.0 -D --logfile iperf3_server2.log
 
 # Serving Client 3
 iperf3 -s -p 5203 -B 0.0.0.0 -D --logfile iperf3_server3.log
+
+#Running Server to capture the messages from Clients and sent to MongoDB
+python3 server.py > server.log 2>&1 &
