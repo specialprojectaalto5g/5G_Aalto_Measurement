2468ec20b8efb515501ad13f525ef2922e38b50f
specialprojectaalto5g <specialprojectaalto5g@gmail.com>
2024-07-22 21:03:20 +0300

    Update server.sh

--- server.sh
+++ server.sh
@@ -9,5 +9,5 @@ iperf3 -s -p 5202 -B 0.0.0.0 -D --logfile iperf3_server2.log
 # Serving Client 3
 iperf3 -s -p 5203 -B 0.0.0.0 -D --logfile iperf3_server3.log
 
-#Running Server to capture the messages from Clients and sent to MongoDB
+#Running Server to capture the messages from Clients and sent to PostGreSQL DB
 python3 final_server.py > server.log 2>&1 &
